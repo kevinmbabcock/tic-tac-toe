83719569b08d2b674691411a6fe146b3669f6715
Kevin Babcock and Malgorzata Haniszewska <malgorzata@>
2017-11-02 11:52:03 -0700

    show marks for individual players with each click on board

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,112 @@
+var player1;
+var player2;
+var currentPlayer;
+var r1c1; var r1c2; var r1c3; var r2c1; var r2c2; var r2c3; var r3c1; var r3c2; var r3c3
+
 function Player (name, mark) {
   this.name = name;
   this.mark = mark;
+  this
+}
+
+function Board (row, column) {
+  this.row = row;
+  this.column = column;
+  this.coordinates = [row, column];
+}
+
+var createBoard = function() {
+  r1c1 = new Board (1,1);
+  r1c2 = new Board (1,2);
+  r1c3 = new Board (1,3);
+  r2c1 = new Board (2,1);
+  r2c2 = new Board (2,2);
+  r2c3 = new Board (2,3);
+  r3c1 = new Board (3,1);
+  r3c2 = new Board (3,2);
+  r3c3 = new Board (3,2);
+}
+
+var playerSwitch = function() {
+  if (currentPlayer === player1) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
+  }
 }
 
 $(document).ready(function() {
   $("#playerNames").submit(function(event) {
     event.preventDefault();
 
-    var player1Name = $("#player1").val();
-    var player2Name = $("#player2").val();
+    var player1Name = $("#namePlayer1").val();
+    var player2Name = $("#namePlayer2").val();
     var player1Mark = "X";
     var player2Mark = "O";
 
-    var player1 = new Player (player1Name, player1Mark);
-    var player2 = new Player (player2Name, player2Mark);
+    player1 = new Player (player1Name, player1Mark);
+    player2 = new Player (player2Name, player2Mark);
+    currentPlayer = player1;
+    $("#currentPlayer").text(currentPlayer.name);
+
+    //createBoard();
 
     $("#playerNames").hide();
     $(".board").show();
     $("body h3").show();
   })
 
+  $(".row1 .column1").click(function() {
+    $(".row1 .column1").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
 
+  $(".row1 .column2").click(function() {
+    $(".row1 .column2").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row1 .column3").click(function() {
+    $(".row1 .column3").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row2 .column1").click(function() {
+    $(".row2 .column1").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row2 .column2").click(function() {
+    $(".row2 .column2").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row2 .column3").click(function() {
+    $(".row2 .column3").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row3 .column1").click(function() {
+    $(".row3 .column1").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row3 .column2").click(function() {
+    $(".row3 .column2").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
+
+  $(".row3 .column3").click(function() {
+    $(".row3 .column3").append("<p>" + currentPlayer.mark + "</p>");
+    playerSwitch();
+    $("#currentPlayer").text(currentPlayer.name);
+  })
 })
